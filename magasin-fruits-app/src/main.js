6cb2015acfae1b17aaf6c3903a539797aff8aa0a
SafiaMaani <saf.maani@gmail.com>
2023-05-02 15:07:30 +0100

    Point d'entré

--- magasin-fruits-app/src/main.js
+++ magasin-fruits-app/src/main.js
@@ -1,9 +1,20 @@
-import Vue from 'vue'
-import App from './App.vue'
-import "@/assets/styles/main.css";
+import Vue from "vue";
+import App from "./App.vue";
+import "./assets/tailwind.css";
+import store from './store/store'
+import NavBar from "./components/NavBar.vue";
+import FruitCard from "./components/FruitCard.vue";
+import CardOnly from "./components/CardOnly.vue";
+import PanierComponant from "./components/PanierComponant.vue";
 
-Vue.config.productionTip = false
+Vue.component("navBar", NavBar);
+Vue.component("fruitCard", FruitCard);
+Vue.component("cardOnly", CardOnly);
+Vue.component("panierComponant", PanierComponant);
+
+Vue.config.productionTip = false;
 
 new Vue({
-  render: h => h(App),
-}).$mount('#app')
+  store,
+  render: (h) => h(App),
+}).$mount("#app");
