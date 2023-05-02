e60759bf547fc861cc4fefe0c5f5b67ca02265a3
SafiaMaani <saf.maani@gmail.com>
2023-05-02 14:53:48 +0100

    Creation de store + la méthode d'ajout de fruit dans le composant

---
+++ magasin-fruits-app/src/store/store.js
@@ -0,0 +1,34 @@
+import Vue from "vue";
+import Vuex from "vuex";
+
+Vue.use(Vuex);
+
+const store = new Vuex.Store({
+  state: {
+    fruitQuantities: {},
+    cartItems: [],
+  },
+  mutations: {
+    addItemToCart(state, item) {
+      // Check if the item already exists in the cart
+      const cartItem = state.cartItems.find(
+        (cartItem) => cartItem.id === item.id
+      );
+
+      // If it exists, increment the quantity property by 1
+      if (cartItem) {
+        cartItem.quantity++;
+        cartItem.total = cartItem.quantity * cartItem.price;
+      } else {
+        // If it doesn't exist, push the new item to the cart
+        const newItem = { ...item, quantity: 1, total: item.price };
+        state.cartItems.push(newItem);
+      }
+    },
+
+  },
+
+  getters: {},
+});
+
+export default store;
