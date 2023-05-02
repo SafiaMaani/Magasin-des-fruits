e1efb1fc5996c42ba21fe8f7898b06a0642fe10a
SafiaMaani <saf.maani@gmail.com>
2023-05-02 15:03:39 +0100

    Ajout du composant FuitCard

---
+++ magasin-fruits-app/src/components/FruitCard.vue
@@ -0,0 +1,15 @@
+<template>
+  <section class="py-6 sm:py-12 dark:bg-gray-800 dark:text-gray-100">
+    <div class="container p-6 mx-auto space-y-8">
+      <div class="space-y-2 text-center">
+        <h2 class="text-3xl font-bold">
+          Bienvenue dans votre magasin de fruits
+        </h2>
+        <p class="font-serif text-sm dark:text-gray-400">
+          Qualisque erroribus usu at, duo te agam soluta mucius.
+        </p>
+      </div>
+      <cardOnly></cardOnly>
+    </div>
+  </section>
+</template>
